446510f5921d5aca68534f05bf822b862b134ed1
zouaghiabderraouf <zouaghiabderraouf91@gmail.com>
2026-05-31 16:09:27 +0100

    feat: initial production stack
    - Server hardening (SSH, UFW, Fail2Ban)
    - Docker Compose stack (App + Nginx)
    - SSL/TLS configuration
    - Logging and Backup automation

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <h1>🚀 DevOps Lab </h1>
+    <p>Container is running!</p>
+    <p>Hostname: ''' + os.popen('hostname').read() + '''</p>
+    '''
+
+@app.route('/health')
+def health():
+    return {'status': 'healthy'}, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
